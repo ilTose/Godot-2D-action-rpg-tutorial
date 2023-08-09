e4a630ba0f58eac14204ec57c0489a733a548519
ilTose <max_tose@hotmail.it>
2023-08-09 17:22:46 +0200

    Ep3: added walk animations to Player

--- player/player.gd
+++ player/player.gd
@@ -1,11 +1,24 @@
 extends CharacterBody2D
 
 @export var speed: int = 35
+@onready var animations = $AnimationPlayer
+
 
 func handle_input():
 	var move_direction = Input.get_vector( "ui_left", "ui_right", "ui_up", "ui_down")
 	velocity = move_direction * speed
 	
+func update_animation():
+	if velocity.length() == 0:
+		animations.stop()
+	else:
+		var direction = "down"
+		if velocity.x < 0: direction = "left"
+		elif velocity.x > 0: direction = "right"
+		elif velocity.y < 0: direction = "up"
+		animations.play("walk_" + direction)
+
 func _physics_process(delta):
 	handle_input()
 	move_and_slide()
+	update_animation()
